7b6453f88f2b0694cbeb932f9db7e0bf0d7617f1
Emmanuel Sayoto <j.sayoto@gmail.com>
2022-12-01 23:24:08 -0500

    large adding, printing, todo: remove

--- A3/src/Ehits.java
+++ A3/src/Ehits.java
@@ -1,5 +1,17 @@
 public class Ehits {
     public static void main(String[] args) throws Exception {
-        
+        Large l = new Large(500000);
+        System.out.println("helo");
+
+        l.add(33240013, "FIRST VALUE");
+        l.add(33240013, "SECOND VALUE");
+        l.add(33240013, "THIRD VALUE");
+        l.add(0, "FIRST VALUE");
+        l.add(1, "FIRST VALUE");
+
+        HashNode<Integer, String> h = l.get(0);
+        System.out.println("GOT: " + h.key + " " +  h.value);
+
+        l.listAll();
     }
 }
--- A3/src/Large.java
+++ A3/src/Large.java
@@ -1,4 +1,5 @@
 public class Large extends ElasticERL {
+    
     //HashTable with Chaining
     
     private HashNode<Integer, String>[] table;
@@ -9,19 +10,21 @@ public class Large extends ElasticERL {
     public Large(int size){
         super();
         table = new HashNode[size];
+        this.size = size;
     }
 
     public void add(int key, String value){
         HashNode<Integer, String> newNode = new HashNode<Integer,String>(key, value, null, null);
-        int bucket = newNode.key % this.size;
+        int bucket = key % this.size;
 
         HashNode<Integer, String> existingNode = table[bucket];
+        System.out.println("bucket: " + bucket);
         if (existingNode == null)
         {
             table[bucket] = newNode;
             tail = newNode;
             head = newNode;
-            size++;
+            
         }
         else
         {
@@ -29,8 +32,55 @@ public class Large extends ElasticERL {
             tail.next = newNode;
             tail.prev = temp;
             tail = tail.next;
-            
-            size++;
+        }
+    }
+
+    public void remove (int key)
+    {
+        int bucket = key % this.size;
+        HashNode<Integer, String> h = get(key);
+        HashNode<Integer, String> temp = h;
+        while (head.next != null)
+        {
+            head = head.next;
+            System.out.println(temp.key + " " + temp.value);
+            h = h.next;
+        }
+
+    }
+
+    public HashNode<Integer, String> get(int key)
+    {
+        int bucket = key % this.size;
+
+        HashNode<Integer, String> h = table[bucket];
+        if (h.key == key)
+        {
+            return h;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void listAll(){
+        for (HashNode<Integer, String> h: table)
+        {
+            if (h == null)
+            {
+                return;
+            }
+
+            System.out.println(h.key + " " + h.value);
+
+            HashNode<Integer, String> temp = h;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+                System.out.println(temp.key + " " + temp.value);
+                h = h.next;
+            }
         }
     }
 }
